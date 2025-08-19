bb3f714b6e3fa1f81813ab1b110ead482212697c
Jumping-Saadman <saadman.omar@gmail.com>
2025-08-19 22:57:24 +0600

    configured tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './pages/**/*.{html,js,jsx,ts,tsx}', // Adjust based on your project structure
+    './components/**/*.{html,js,jsx,ts,tsx}',
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
